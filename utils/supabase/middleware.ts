b94a757051646d147a63d522baf645bf5790405d
Jeronimo Contreras <jeronimocontreras@Jeronimos-MacBook-Pro-9.local>
2026-03-28 19:27:55 -0400

    login change

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -4,7 +4,7 @@ import { type NextRequest, NextResponse } from "next/server";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
-console.log('supabaseUrl', supabaseUrl);
+console.warn('supabaseUrl', supabaseUrl);
 
 export const createClient = (request: NextRequest) => {
   // Create an unmodified response
